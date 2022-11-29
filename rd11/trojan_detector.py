bf2e65f27c84b6567ea3dc2cee3c81ced4a5e558
Matthew Yudin <Matthew.Yudin@perspectalabs.com>
2022-11-28 23:09:28 -0500

    60 features

--- rd11/trojan_detector.py
+++ rd11/trojan_detector.py
@@ -323,7 +323,10 @@ def train_model(data, summary_size):
     clf = clf_rf.fit(X_train, y)
     importance = np.argsort(clf.feature_importances_)[-60:]
     X = np.concatenate((X_train[:,importance], X[:,-1*summary_size:]), axis=1)
-    clf_svm = SVC(probability=True, kernel='linear')
+    clf_svm = SVC(probability=True, kernel='rbf')
+    parameters = {'gamma':[0.01,0.1], 'C':[0.1,1,10,100]}
+    #clf_svm = GridSearchCV(clf_svm, parameters)
+    clf_svm = BaggingClassifier(base_estimator=clf_svm, n_estimators=6, max_samples=0.83, bootstrap=False)
     X = sc.fit_transform(X)
     clf_svm.fit(X,y)
 
