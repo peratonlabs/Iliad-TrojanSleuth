69034c9e6943260cd70d13182c64934235b63344
Matthew Yudin <Matthew.Yudin@perspectalabs.com>
2022-12-16 09:11:45 -0500

    add summary statistics

--- rd11/trojan_detector.py
+++ rd11/trojan_detector.py
@@ -63,8 +63,8 @@ def trojan_detector(model_filepath,
         if features != None:
 
             features = np.array(features.detach().cpu()).reshape(1,-1)
-            #features_full = np.concatenate((features[:,overall_importances], features[:,-1*summary_size:]), axis=1)
-            trojan_probability = clf.predict_proba(scale(scaler.transform(features[:,overall_importances]),axis=1))[0][1]
+            features_full = np.concatenate((features[:,overall_importances], features[:,-1*summary_size:]), axis=1)
+            trojan_probability = clf.predict_proba(scale(scaler.transform(features_full),axis=1))[0][1]
 
             logging.info('Trojan Probability: {}'.format(trojan_probability))
 
@@ -252,8 +252,8 @@ def train_model(data, summary_size):
     clf_rf = RandomForestClassifier(n_estimators=500)
     clf_lr = LogisticRegression()
     clf = clf_rf.fit(X_train, y)
-    importance = np.argsort(clf.feature_importances_)[-500:]
-    #X = np.concatenate((X_train[:,importance], X[:,-1*summary_size:]), axis=1)
+    importance = np.argsort(clf.feature_importances_)[-1000:]
+    X = np.concatenate((X_train[:,importance], X[:,-1*summary_size:]), axis=1)
     clf_svm = SVC(probability=True, kernel='rbf')
     parameters = {'gamma':[0.001,0.005,0.01,0.02], 'C':[0.1,1,10,100]}
     clf_rf = RandomForestClassifier(n_estimators=500)
@@ -261,7 +261,7 @@ def train_model(data, summary_size):
     clf_svm = GridSearchCV(clf_svm, parameters)
     eclf = VotingClassifier(estimators=[('rf', clf_rf), ('svm', clf_svm)], voting='soft')
     #clf_svm = BaggingClassifier(base_estimator=clf_svm, n_estimators=6, max_samples=0.83, bootstrap=False)
-    X = sc.fit_transform(X_train[:,importance])
+    X = sc.fit_transform(X)
     X = scale(X, axis=1)
     clf_rf.fit(X,y)
 
