13d8b5bc09f5640a796bfc295965cba41ee07d39
Matthew Yudin <Matthew.Yudin@perspectalabs.com>
2023-01-11 09:32:04 -0500

    clip predicted probability

--- rd12/detector.py
+++ rd12/detector.py
@@ -179,10 +179,10 @@ class Detector(AbstractDetector):
         return estimator.score(X, y)
 
     def custom_scoring_function(self, estimator, X, y):
-        return roc_auc_score(y, estimator.predict_proba(X)[:,1])
+        return roc_auc_score(y, np.clip(estimator.predict_proba(X)[:,1], 0.01, 0.99))
         
     def custom_loss_function(self, estimator, X, y):
-        return log_loss(y, estimator.predict_proba(X)[:,1])
+        return log_loss(y, np.clip(estimator.predict_proba(X)[:,1], 0.01, 0.99))
 
     def inference_on_example_data(self, model, examples_dirpath):
         """Method to demonstrate how to inference on a round's example data.
@@ -267,7 +267,7 @@ class Detector(AbstractDetector):
         with open(join(self.learned_parameters_dirpath, "clf.joblib"), "rb") as fp:
             clf = pickle.load(fp)
 
-        trojan_probability = clf.predict_proba(results)[0][1]
+        trojan_probability = np.clip(clf.predict_proba(results)[0][1], 0.01, 0.99)
 
         probability = str(trojan_probability)
         with open(result_filepath, "w") as fp:
