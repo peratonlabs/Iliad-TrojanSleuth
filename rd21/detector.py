87f2356d96dddacec828f22bf86cef7d4c3880a8
Yudin <myudin@ats.atsinnovate.com>
2024-09-20 17:10:36 -0400

    Threshold of less than 0.1

--- rd21/detector.py
+++ rd21/detector.py
@@ -205,7 +205,7 @@ class Detector(AbstractDetector):
         model, model_repr, model_class = load_model(model_filepath)
         bias_score = np.mean(model_repr['fc_2.bias'])
 
-        if bias_score > 0:
+        if bias_score < 0.1:
             probability = 0.75
         else:
             probability = 0.25
